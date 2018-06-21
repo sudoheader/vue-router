aca02bc1e333bfdd4d9673613e36a29f333e4059
Raymond Nazaryan <rayn2427@gmail.com>
2018-06-21 13:16:25 -0700

    Added beforeEach guard

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -25,6 +25,13 @@
                     hash: '#data'
                 }
             }
+        },
+        beforeRouteEnter(to, from, next) {
+            if (true) {
+                next();
+            } else {
+                next(false);
+            }
         }
     }
 </script>
--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -3,6 +3,7 @@
         <h3>Edit the User</h3>
         <p>Locale: {{ $route.query.locale }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
+        <button class="btn btn-primary">Confirm</button>
         <div style="height: 700px"></div>
         <p>Some extra Data</p>
     </div>
